a89dc3acccde6c88a40d483329f34f9c5a73e57f
Lizeth Daniela Castellanos Alfonso <ld.castellanos@uniandes.edu.co>
2023-05-09 14:15:48 -0500

    Feature/player base (#4)
    * Add bullet creation and configuration
    
    * Enable bullet screen bound system
    
    * Add bullet on player

---
+++ src/ecs/components/tags/c_tag_bullet_static.py
@@ -0,0 +1,3 @@
+class CTagBulletStatic:
+    def __init__(self):
+        pass
--- src/ecs/systems/s_rendering.py
+++ src/ecs/systems/s_rendering.py
@@ -5,10 +5,12 @@ import pygame
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_surface import CSurface
 
-def system_rendering(world:esper.World, screen:pygame.Surface):
+
+def system_rendering(world: esper.World, screen: pygame.Surface):
     components = world.get_components(CTransform, CSurface)
-    
-    c_t:CTransform
-    c_s:CSurface
+
+    c_t: CTransform
+    c_s: CSurface
     for _, (c_t, c_s) in components:
-        screen.blit(c_s.surf, c_t.pos, area=c_s.area)
+        if c_s.show:
+            screen.blit(c_s.surf, c_t.pos, area=c_s.area)
