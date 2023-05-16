d03553ce65357837ae45b28613d5b108510da51e
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-05-16 14:50:12 -0400

    se agrega logo

--- src/game/menu_scene.py
+++ src/game/menu_scene.py
@@ -1,8 +1,10 @@
 import pygame
+import esper
+from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 
 from src.engine.scenes.scene import Scene
-from src.create.prefab_creator_interface import TextAlignment, create_text
+from src.create.prefab_creator_interface import TextAlignment, create_text, create_image
 from src.ecs.components.c_input_command import CInputCommand 
 
 class MenuScene(Scene):
@@ -11,10 +13,17 @@ class MenuScene(Scene):
         # Crea las entidades de texto y asigna a los atributos correspondientes
         self.title = create_text(self.ecs_world, "MAIN MENU", 12, 
                     pygame.Color(50, 255, 50), pygame.Vector2(128, 200), TextAlignment.CENTER)
+        
+        self.logo = create_image(self.ecs_world, "./assets/img/invaders_logo_title.png",
+                                pygame.Vector2(55, 220))
+        
         self.start = create_text(self.ecs_world, "PRESS Z TO START GAME", 9, 
-                    pygame.Color(255, 255, 0), pygame.Vector2(128, 240), TextAlignment.CENTER)
+                    pygame.Color(255, 255, 0), pygame.Vector2(128, 275), TextAlignment.CENTER)
+        
         self.controls = create_text(self.ecs_world, "Arrows to MOVE - P to PAUSE", 7, 
-                    pygame.Color(150, 150, 255), pygame.Vector2(128, 267), TextAlignment.CENTER)
+                    pygame.Color(150, 150, 255), pygame.Vector2(128, 290), TextAlignment.CENTER)
+        
+        
 
         # Define la velocidad de movimiento de las entidades de texto
         self.speed = pygame.Vector2(0, -40)
@@ -24,7 +33,7 @@ class MenuScene(Scene):
         self.ecs_world.add_component(start_game_action,CInputCommand("START_GAME", pygame.K_z))
         
     def do_update(self, delta_time):
-        for text_entity in [self.title, self.start, self.controls]:
+        for text_entity in [self.logo, self.title, self.start, self.controls]:
             text_transform = self.ecs_world.component_for_entity(text_entity, CTransform)
             text_transform.pos += self.speed * delta_time
 
