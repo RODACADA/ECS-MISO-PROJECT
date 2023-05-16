892a88c526cefa8c4dda1a6c9db735dc1a08759d
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-05-16 01:21:53 -0400

    Se ajustan textos y size

--- src/engine/services/fonts_service.py
+++ src/engine/services/fonts_service.py
@@ -2,10 +2,12 @@ import pygame
 
 
 class FontsService:
-    def __init__(self):
+    def __init__(self) -> None:
         self._fonts = {}
 
     def get(self, path: str, size: int) -> pygame.font.FontType:
         if path not in self._fonts:
-            self._fonts[path] = pygame.font.Font(path, size)
-        return self._fonts[path]
+            self._fonts[(path,size)] = pygame.font.Font(path, size)
+        return self._fonts[(path,size)]
+    
+    
--- src/game/menu_scene.py
+++ src/game/menu_scene.py
@@ -7,12 +7,12 @@ from src.ecs.components.c_input_command import CInputCommand
 class MenuScene(Scene):
     
     def do_create(self):
-        create_text(self.ecs_world, "MAIN MENU", 16, 
-                    pygame.Color(50, 255, 50), pygame.Vector2(320, 150), TextAlignment.CENTER)
-        create_text(self.ecs_world, "PRESS Z TO START GAME", 11, 
-                    pygame.Color(255, 255, 0), pygame.Vector2(320, 210), TextAlignment.CENTER)
-        create_text(self.ecs_world, "Arrows to MOVE - P to PAUSE", 8, 
-                    pygame.Color(150, 150, 255), pygame.Vector2(320, 250), TextAlignment.CENTER)
+        create_text(self.ecs_world, "MAIN MENU", 12, 
+                    pygame.Color(50, 255, 50), pygame.Vector2(128, 100), TextAlignment.CENTER)
+        create_text(self.ecs_world, "PRESS Z TO START GAME", 9, 
+                    pygame.Color(255, 255, 0), pygame.Vector2(128, 140), TextAlignment.CENTER)
+        create_text(self.ecs_world, "Arrows to MOVE - P to PAUSE", 7, 
+                    pygame.Color(150, 150, 255), pygame.Vector2(128, 167), TextAlignment.CENTER)
         
         start_game_action = self.ecs_world.create_entity()
         self.ecs_world.add_component(start_game_action,
